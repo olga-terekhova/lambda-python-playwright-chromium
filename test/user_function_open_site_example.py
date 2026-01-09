173cef7b05aeaa1dbbddb93848bc6ddd1f596406
Olga Terekhova <97198539+olga-terekhova@users.noreply.github.com>
2026-01-09 11:50:49 -0500

    Save screenshot and DOM-tree for the URL passed in parameters

--- test/user_function_open_site_example.py
+++ test/user_function_open_site_example.py
@@ -1,3 +1,6 @@
+import traceback
+import boto3
+
 def get_chromium_on_aws_lambda_flags():
     return [
         "--autoplay-policy=user-gesture-required",
@@ -38,25 +41,84 @@ def get_chromium_on_aws_lambda_flags():
         "--use-mock-keychain",
     ]
 
-def OpenSite():
+def OpenSite(ID, URL, bucket, output_dir):
+    
     from playwright.sync_api import sync_playwright
 
-    with sync_playwright() as p:
-        # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
-        browser = p.chromium.launch(headless=True, 
-        #                            user_agent=user_agent,
-                                    args= get_chromium_on_aws_lambda_flags() )  # Launch Chromium browser. Arguments are important for running within AWS Lambda. 
-        
-        # Desktop Chrome device settings
-        desktop_chrome = p.devices["Desktop Chrome"]
+    key_dir = output_dir + "/" + ID + "/"
+    screenshot_key = key_dir +"screenshot.png"
+    page_key = key_dir + "page.html"
+    s3 = boto3.client("s3")
 
-        # Create browser context with Desktop Chrome emulation
-        context = browser.new_context(
-            **desktop_chrome
-        )
-        
-        page = context.new_page()       # Create a new page (tab)
-        page.goto("https://www.example.com") # Navigate to a URL
-        str = page.title()              # Print the page title
-        browser.close()                 # Close the browser
-    return f"Title is: {str}"
+    s = """
+    if (navigator.webdriver === false) {
+    // Post Chrome 89.0.4339.0 and already good
+    } else if (navigator.webdriver === undefined) {
+    // Pre Chrome 89.0.4339.0 and already good
+    } else {
+    // Pre Chrome 88.0.4291.0 and needs patching
+    delete Object.getPrototypeOf(navigator).webdriver
+    }
+
+    """
+ 
+    
+    try:
+        with sync_playwright() as p:
+            
+            browser = p.chromium.launch(headless=True, 
+                                        args= get_chromium_on_aws_lambda_flags() )  # Launch Chromium browser. Arguments are important for running within AWS Lambda. 
+            
+            # Desktop Chrome device settings
+            desktop_chrome = p.devices["Desktop Chrome"]
+            extra_headers = {
+                "Sec-CH-UA": '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
+                "Accept-Language": "en-US,en;q=0.9,ru;q=0.8,fr-FR;q=0.7,fr;q=0.6",
+                "Sec-Fetch-Site": "cross-site"
+            }
+
+            # Create browser context with Desktop Chrome emulation
+            context = browser.new_context(
+                **desktop_chrome,
+                extra_http_headers=extra_headers 
+            )
+            
+            # Create page and pass initialization script
+            page = context.new_page()      
+            page.add_init_script(s)
+
+            # Navigate to the URL
+            page.goto(URL, wait_until="load") 
+
+            # Take screenshot into memory (bytes)
+            screenshot_bytes = page.screenshot(full_page=True)
+
+            # Upload to S3
+            s3.put_object(
+                Bucket=bucket,
+                Key=screenshot_key,
+                Body=screenshot_bytes,
+                ContentType="image/png"
+            )
+
+            # Get full DOM as serialized HTML
+            dom_html = page.content()
+
+            # Upload to S3
+            s3.put_object(
+                Bucket=bucket,
+                Key=page_key,
+                Body=dom_html.encode("utf-8"),
+                ContentType="text/html; charset=utf-8"
+            )
+
+            str = f"Success: The screenshot and the DOM-tree for {page.title()} were put in {key_dir}"               # Print the page title
+            browser.close()                 # Close the browser
+    
+    except Exception as e:
+        tb = traceback.print_exc()
+        str = f"Error: {str(e)} {tb}"
+               
+
+
+    return str
